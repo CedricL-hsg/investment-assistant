f8fb0773171fa666ba8e5b98d6298df112abf34e
CedricL-hsg <cedric-j.lampert@bluewin.ch>
2025-04-14 09:29:17 +0200

    Delete data directory

--- data/data_fetcher.py
+++
@@ -1,5 +0,0 @@
-import yfinance as yf
-
-def get_price_data(ticker, period="5y", interval="1d"):
-    stock = yf.Ticker(ticker)
-    return stock.history(period=period, interval=interval)
--- data/indicators.py
+++
@@ -1,8 +0,0 @@
-def calculate_max_drawdown(prices):
-    """
-    Berechnet den maximalen Drawdown einer Preisreihe.
-    """
-    peak = prices.cummax()
-    drawdown = (prices - peak) / peak
-    max_drawdown = drawdown.min()
-    return max_drawdown
